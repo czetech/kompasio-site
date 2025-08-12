2a8b2dfe777a6f6f6d0150b468c9f5f6da645968
Petr Czepiec <petr@czepiec.me>
2025-08-12 15:41:34 +0200

    Add contact page

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,27 +1,17 @@
 import type { Component } from "solid-js";
 import { splitProps } from "solid-js";
 import { Dynamic } from "solid-js/web";
+import Circle from "~/components/Circle.tsx"
 
 const Button: Component = (props) => {
-  const [, otherProps] = splitProps(props, ["class", "classList", "secondary"]);
+  const [, otherProps] = splitProps(props, ["icon"]);
 
-  const classCommon = `disabled:bg-frontier-gray rounded-sm border px-3 py-3
-    font-medium enabled:cursor-pointer md:px-6`;
-  const classPrimary = `bg-red-orange hover:bg-red-orange-light
-    active:bg-red-orange-dark text-shuttle-white border-transparent`;
-  const classSecondary = `bg-shuttle-white hover:bg-shuttle-white-light
-    active:bg-shuttle-white-dark text-space-blue border-red-orange`;
-  const classList = () => ({
-    [classCommon]: true,
-    [classPrimary]: !props.secondary,
-    [classSecondary]: props.secondary,
-    [props.class]: props.class,
-    ...props.classList,
-  });
-  const a = () => <a classList={classList()} {...otherProps} />;
-  const button = () => <button classList={classList()} {...otherProps} />;
-
-  return <Dynamic component={otherProps.href ? a : button} />;
+  return (
+    <a class="border-y-2 border-r-2 rounded-full inline-flex h-8 items-center" {...otherProps}>
+      <Circle>{props.icon}</Circle>
+      <span class="pl-4 pr-6">{props.text}</span>
+    </a>
+  );
 };
 
 export default Button;
