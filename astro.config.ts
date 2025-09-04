c3221015d3d5e9644975e3899e22db6272e981fb
Petr Czepiec <petr@czepiec.me>
2025-09-04 18:11:33 +0200

    Test PWA

--- astro.config.ts
+++ astro.config.ts
@@ -2,13 +2,14 @@ import solidJs from "@astrojs/solid-js";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 import node from "@astrojs/node";
+import AstroPWA from '@vite-pwa/astro';
 
 export default defineConfig({
   adapter: node({
     mode: "standalone",
   }),
-  //trailingSlash: "never",
-  integrations: [solidJs()],
+  trailingSlash: "never",
+  integrations: [AstroPWA({}),solidJs()],
   vite: {
     plugins: [tailwindcss()],
   },
