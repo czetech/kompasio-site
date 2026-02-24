accdd4b57471e55fab945277d9756cca60b07998
Petr Czepiec <petr@czepiec.me>
2026-02-24 20:26:59 +0100

    Add cookie consent

--- public/astro.config.mjs
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from "astro/config";
-import node from "@astrojs/node";
-// https://astro.build/config
-export default defineConfig({
-  adapter: node({
-    mode: "standalone",
-  }),
-});
